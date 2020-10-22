6e653c3d35de1fcc96502e31c49821ac7de3fb7b
ravipandey-dev <ravipan31.1991@gmail.com>
2020-10-22 08:40:57 +0530

    initial commit

---
+++ src/main/java/com/test/filePoller/FilePollerApplication.java
@@ -0,0 +1,13 @@
+package com.test.filePoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilePollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/filePoller/FilePollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.filePoller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilePollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
